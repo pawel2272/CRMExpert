f277bc31273b82b071bb72f7811f580afcddb4fa
pawel2272 <pawel2272@gmail.com>
2021-10-23 17:28:36 +0200

    dependency resolver class implementation

--- HRBN.Thesis.CRMExpert.Application/ContainerModules/AutofacDependencyResolver.cs
+++ HRBN.Thesis.CRMExpert.Application/ContainerModules/AutofacDependencyResolver.cs
@@ -1,7 +1,19 @@
+using Autofac;
+using HRBN.Thesis.CRMExpert.Application.Core.Mediator;
+
 namespace HRBN.Thesis.CRMExpert.Application.ContainerModules
 {
-    public class AutofacDependencyResolver
+    public class AutofacDependencyResolver : IDependencyResolver
     {
-        
+        private readonly ILifetimeScope _lifetimeScope;
+
+        public AutofacDependencyResolver(ILifetimeScope lifetimeScope)
+        {
+            _lifetimeScope = lifetimeScope;
+        }
+        public T ResolveOrDefault<T>() where T : class
+        {
+            return _lifetimeScope.ResolveOptional<T>();
+        }
     }
 }
