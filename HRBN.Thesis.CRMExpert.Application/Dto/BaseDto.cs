b12ef6b080815e7388d7647685921bf65fb03001
pawel2272 <pawel2272@gmail.com>
2021-10-24 23:09:19 +0200

    addition of Dto

--- HRBN.Thesis.CRMExpert.Application/Dto/BaseDto.cs
+++
@@ -1,7 +0,0 @@
-namespace HRBN.Thesis.CRMExpert.Application.Dto
-{
-    public class BaseDto
-    {
-        
-    }
-}
---
+++ HRBN.Thesis.CRMExpert.Application/Dto/RoleDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HRBN.Thesis.CRMExpert.Application.Dto
+{
+    public class RoleDto
+    {
+        public Guid Id { get; protected set; }
+        public string Name { get; protected set; }
+    }
+}
