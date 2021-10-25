cac009ebcf969fd8a74ba89a45b7295523fc7856
pawel2272 <pawel2272@gmail.com>
2021-10-25 08:32:22 +0200

    addition of mediator module

--- HRBN.Thesis.CRMExpert.Application/ContainerModules/MediatorModule.cs
+++ HRBN.Thesis.CRMExpert.Application/ContainerModules/MediatorModule.cs
@@ -1,6 +1,17 @@
+using Autofac;
+using AutoMapper;
+using HRBN.Thesis.CRMExpert.Application.Core.Command;
+using HRBN.Thesis.CRMExpert.Application.Core.Mediator;
+using HRBN.Thesis.CRMExpert.Application.Core.Query;
+using HRBN.Thesis.CRMExpert.Application.CRMExpertDefinitions.Commands.Contact;
+using HRBN.Thesis.CRMExpert.Application.Mappers.Profiles;
+using HRBN.Thesis.CRMExpert.Domain.Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+
 namespace HRBN.Thesis.CRMExpert.Application.ContainerModules
 {
-    public class MediatorModule
+    public static class MediatorModule
     {
         public static void ConfigureMediator(this ContainerBuilder containerBuilder)
         {
