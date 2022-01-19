7cb32c9a17476bd9530ad96bb5901eaaf225c202
Paweł Harbuz <pawel.harbuz@spicasolutions.pl>
2022-01-19 17:49:59 +0100

    views upgrade

---
+++ HRBN.Thesis.CRMExpert.UI/Areas/User/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRBN.Thesis.CRMExpert.UI.Areas.User.Controllers
+{
+    [Area("User")]
+    public class OrderController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
