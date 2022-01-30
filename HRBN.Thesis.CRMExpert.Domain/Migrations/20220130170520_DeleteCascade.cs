8ce56bf5fd257d99b9dc3ed3fcd51344f95d20dd
Paweł Harbuz <pawel2272@gmail.com>
2022-01-30 18:06:10 +0100

    delete cascade migration add

---
+++ HRBN.Thesis.CRMExpert.Domain/Migrations/20220130170520_DeleteCascade.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRBN.Thesis.CRMExpert.Domain.Migrations
+{
+    public partial class DeleteCascade : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
